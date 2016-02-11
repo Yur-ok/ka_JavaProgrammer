2206676914237b32b0acc45179e389f6de71dbeb
Yurok <Materik-r@yandex.ru>
2016-02-11 23:44:39 +0700

    Done frame 4

---
+++ src/Lesson_01/kp_02/frame_04/PrintTankInfo.java
@@ -0,0 +1,18 @@
+package Lesson_01.kp_02.frame_04;
+
+/**
+ * Created by Юрий on 11.02.2016.
+ */
+public class PrintTankInfo {
+
+    static void printTankInfo() {
+        Tank t = new Tank();
+        System.out.println("Tank information:\nColor = " + t.color);
+        System.out.println("Crew equal = " + t.crew);
+        System.out.println("Max speed equal = " + t.maxSpeed);
+    }
+
+    public static void main(String[] args) {
+        printTankInfo();
+    }
+}
---
+++ src/Lesson_01/kp_02/frame_04/Tank.java
@@ -0,0 +1,11 @@
+package Lesson_01.kp_02.frame_04;
+
+/**
+ * Created by Юрий on 11.02.2016.
+ */
+public class Tank {
+
+    String color;
+    int crew;
+    int maxSpeed;
+}
