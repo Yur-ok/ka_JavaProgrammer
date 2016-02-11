014b4c0c2711ca523d1030e21bfaac28cd87ed69
Yurok <Materik-r@yandex.ru>
2016-02-11 21:52:49 +0700

    Учусь использвоать GitHub. Добавил файлы проекта

---
+++ src/Lesson_01/GreetingService_1_1_5.java
@@ -0,0 +1,24 @@
+package Lesson_01;
+
+/**
+ * Created by Юрий on 10.02.2016.
+ */
+public class GreetingService_1_1_5 {
+    public static void main(String[] args) {
+
+//        System.out.println(args.length);
+
+        if (args.length > 0) {
+            System.out.print("Hello ");
+            for (int i = 0; i <= args.length - 1; i++) {
+                if (i == args.length - 1) {
+                    System.out.println(args[i]);
+                } else {
+                    System.out.print(args[i] + " ");
+                }
+            }
+        } else {
+            System.out.println("Type one parameter after class name! \nExample:\njava GreetingService_1_1_5 [some text]");
+        }
+    }
+}
