8630c1f826f299621edb1b1ebffaee4868f953af
Yurok <Materik-r@yandex.ru>
2016-02-14 17:51:14 +0700

    kp_04 - frame_02 создал файлы заготовки

---
+++ src/Lesson_01/kp_04/frame_02/ActionField.java
@@ -0,0 +1,7 @@
+package Lesson_01.kp_04.frame_02;
+
+/**
+ * Created by Юрий on 12.02.2016.
+ */
+public class ActionField {
+}
---
+++ src/Lesson_01/kp_04/frame_02/Launcher.java
@@ -0,0 +1,10 @@
+package Lesson_01.kp_04.frame_02;
+
+/**
+ * Created by Юрий on 12.02.2016.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+
+    }
+}
