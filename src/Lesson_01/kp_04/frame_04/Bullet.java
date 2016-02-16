51ac9924f5043cbabccc226310eccc2215e0c71b
Yur-ok <Materik-r@yandex.ru>
2016-02-16 23:43:43 +0700

    kp_04 - frame_04 мой вариант ПОСЛЕ просмотра видео от Олега забыл добавить конструктор, исправляюсь!

--- src/Lesson_01/kp_04/frame_04/Bullet.java
+++ src/Lesson_01/kp_04/frame_04/Bullet.java
@@ -10,6 +10,11 @@ public class Bullet {
     private int y;
     private int direction;
 
+    public Bullet(int x, int y, int direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
 
     public int getX() {
         return x;
