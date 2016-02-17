b00ba8ff896f2a651168f93a43679258a1499d11
Yur-ok <Materik-r@yandex.ru>
2016-02-17 22:30:08 +0700

    kp_04 - frame_12 мой вариант ПОСЛЕ просмотра видео от Олега

--- src/Lesson_01/kp_04/frame_12/Tank.java
+++ src/Lesson_01/kp_04/frame_12/Tank.java
@@ -13,35 +13,27 @@ public class Tank {
     private int LEFT = 3;
     private int RIGHT = 4;
 
-    private int Direction;
-    private int X;
-    private int Y;
+    private int direction;
+    private int x;
+    private int y;
     private int speed = 10;
     private ActionField af;
     private BattleField bf;
 
-    public Tank() {
-
+    public Tank(ActionField af, BattleField bf) {
+        this(af, bf, 128, 512, 1);
     }
 
-    public ActionField getActionField() {
-        return af;
-    }
-
-    public void setActionField(ActionField af) {
+    public Tank(ActionField af, BattleField bf, int x, int y, int direction) {
         this.af = af;
-    }
-
-    public BattleField getBattleField() {
-        return bf;
-    }
-
-    public void setBattleField(BattleField bf) {
         this.bf = bf;
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
     }
 
     public int getDirection() {
-        return Direction;
+        return direction;
     }
 
     public int getSpeed() {
@@ -49,84 +41,25 @@ public class Tank {
     }
 
     public int getX() {
-        return X;
-    }
-
-    public void setX(int x) {
-        X = x;
+        return x;
     }
 
     public int getY() {
-        return Y;
+        return y;
     }
 
-    public void setY(int y) {
-        Y = y;
+    public void turn(int direction) throws Exception {
+        this.direction = direction;
+        af.processTurn(this);
     }
 
-    public void turn(int direction) {
-        if (Direction != direction) {
-            Direction = direction;
-        }
+    public void move() throws Exception {
+        af.processMove(this);
     }
-
-    public void move(int direction) throws Exception {
-        int step = 1;
-        int covered = 0;
-
-        // check limits x: 0, 512; y: 0, 512
-        if ((direction == UP && Y == 0) || (direction == DOWN && Y >= 512)
-                || (direction == LEFT && X == 0) || (direction == RIGHT && X >= 512)) {
-            System.out.println("[illegal move] direction: " + direction + " tankX: " + X + ", tankY: " + Y);
-            return;
-        }
-
-        turn(direction);
-
-        while (covered < 64) {
-            if (direction == UP) {
-                Y -= step;
-                System.out.println("[move up] direction: " + direction + " tankX: " + X + ", tankY: " + Y);
-            } else if (direction == DOWN) {
-                Y += step;
-                System.out.println("[move down] direction: " + direction + " tankX: " + X + ", tankY: " + Y);
-            } else if (direction == LEFT) {
-                X -= step;
-                System.out.println("[move left] direction: " + direction + " tankX: " + X + ", tankY: " + Y);
-            } else {
-                X += step;
-                System.out.println("[move right] direction: " + direction + " tankX: " + X + ", tankY: " + Y);
-            }
-            covered += step;
-            actionField;
-            Thread.sleep(speed);
-        }
-    }
-
 
     public void fire() throws Exception {
-
-        Bullet bullet = new Bullet(-100, -100, -1);
-        bullet.updateX(25);
-        bullet.updateY(25);
-
-        while ((bullet.getX() > -14 && bullet.getX() < 592) && (bullet.getY() > -14 && bullet.getY() < 592)) {
-            if (Direction == UP) {
-                bullet.updateY(-1);
-            } else if (Direction == DOWN) {
-                bullet.updateY(+1);
-            } else if (Direction == LEFT) {
-                bullet.updateX(-1);
-            } else if (Direction == RIGHT) {
-                bullet.updateX(+1);
-            }
-            repaint();
-            Thread.sleep(bullet.getSpeed());
-            if (processInterception("bullet")) {
-                bullet.destroy();
-                amountOfBriks--;
-            }
-        }
+        Bullet bullet = new Bullet((x + 25), (y + 25), direction);
+        af.processFire(bullet);
     }
 
     public void moveRandom() {
@@ -135,5 +68,17 @@ public class Tank {
 
     public void moveToQuadrant() {
         // TODO: 17.02.2016
+    }
+
+    public void clean() throws Exception {
+
+    }
+
+    public void updateX(int x) {
+        this.x += x;
+    }
+
+    public void updateY(int y) {
+        this.y += y;
     }
 }
