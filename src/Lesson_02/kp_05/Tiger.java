a870e2d5e9e1c73d9ced02ed107f36d4731d12c1
Yur-ok <Materik-r@yandex.ru>
2016-02-29 12:29:28 +0700

    Создал два танка и внес необходимые изменения

--- src/Lesson_02/kp_05/Tiger.java
+++ src/Lesson_02/kp_05/Tiger.java
@@ -5,20 +5,5 @@ package Lesson_02.kp_05;
  */
 public class Tiger extends Tank {
 
-    public Tiger() {
-        setColor("Orange");
-        setCrew(4);
-        setSpeed(56);
-    }
-
-    @Override
-    public void move() throws Exception {
-        System.out.println("Tiger is moving with " + getSpeed() + " km/h");
-    }
-
-    @Override
-    public String toString() {
-        return "Tiger has next parameters: \nColor = " + getColor() + "\nSpeed = " + getSpeed() + "\nCrew = " + getCrew()
-                + "\n";
-    }
+    private int armor;
 }
