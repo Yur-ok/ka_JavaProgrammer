916690d4d95c002551d24b077c80bc5815233f18
Yur-ok <Materik-r@yandex.ru>
2016-04-07 20:51:41 +0700

    Визуализировал фигуры и добавил обводку, вот только странно как-то цвета назначились этой обводке...

--- src/Lesson_03/kp_02/shape/Circle.java
+++ src/Lesson_03/kp_02/shape/Circle.java
@@ -8,6 +8,8 @@ import java.awt.*;
 public class Circle extends Shape {
     @Override
     public void draw(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setStroke(new BasicStroke(10));
         g.drawOval(30, 30, 100, 100);
         g.setColor(new Color(255, 189, 61));
         g.fillOval(30, 30, 100, 100);
