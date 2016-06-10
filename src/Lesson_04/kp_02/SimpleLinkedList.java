4d270813abc4fe8baf62e35735f77f869b1ba9fd
Yur-ok <Materik-r@yandex.ru>
2016-06-10 15:03:57 +0700

    собственная реализация списка LinkedList

---
+++ src/Lesson_04/kp_02/SimpleLinkedList.java
@@ -0,0 +1,27 @@
+package Lesson_04.kp_02;
+
+import Lesson_01.kp_02.frame_15.rigth_Oleg.Object;
+
+/**
+ * Created by Юрий on 10.06.2016.
+ */
+public class SimpleLinkedList extends Node {
+    private Node root;
+    private int size;
+
+    public void addFirst(Object o) {
+
+    }
+
+    public void addLast(Object o) {
+
+    }
+
+    public void addAfter(Object o) {
+
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
